ff948a3a5600dba5a8179c09d952e69316a92dd7
Sheila Chebii <shillerchier055@gmail.com>
2024-01-08 17:12:49 +0300

    done

---
+++ app1/migrations/versions/39c15120a361_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 39c15120a361
+Revises: 
+Create Date: 2024-01-08 17:08:57.266512
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '39c15120a361'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
